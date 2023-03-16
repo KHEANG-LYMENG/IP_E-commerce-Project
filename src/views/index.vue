a5334162de8337178d77a9fced5e2aa700da30e5
KHEANG-LYMENG <lymengkheang1307@gmail.com>
2023-03-16 09:09:51 +0700

    Initialize E-commerce *Homepage *Aboutpage *productpage *ongoing checkout page

---
+++ src/views/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/product">Product</RouterLink>
+        <button @click="$emit('checkout')"></button>
+      </nav>
+    </div>
+    <RouterView />
+  </header>
+</template>
