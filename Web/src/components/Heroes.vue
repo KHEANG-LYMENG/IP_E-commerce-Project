8fba2bc7e7a9b822339b9c7563171311d5eb122e
KHEANG-LYMENG <lymengkheang1307@gmail.com>
2023-07-07 15:33:19 +0700

    V2

--- Web/src/components/Heroes.vue
+++ Web/src/components/Heroes.vue
@@ -8,7 +8,7 @@
           <h1>Get Ready !!</h1><br><br>
           <h2>Fresh Start with New Season</h2>
           <h2>With Special Discount</h2><br><br>
-          <h2>Up To <span style="color: red;">%20</span></h2><br><br><br>
+          <h2>Up To <span style="color: red;">%20 off</span></h2><br><br><br>
           <button style="background-color: blue; color: white;" ><router-link to="/home" class="switch">SHOP NOW</router-link></button>
         </div>
       </div>
